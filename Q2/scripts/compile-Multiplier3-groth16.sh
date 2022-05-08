0dacbfd331338ef8acceaafae0cc95efdcc7e43b
Njunu-sk <simonnjunu16@gmail.com>
2022-05-08 13:42:48 +0300

    minor edits

--- Q2/scripts/compile-Multiplier3-groth16.sh
+++ Q2/scripts/compile-Multiplier3-groth16.sh
@@ -4,13 +4,6 @@ cd contracts/circuits
 
 mkdir Multipler3
 
-if [ -f ./powersOfTau28_hez_final_10.ptau ]; then
-    echo "powersOfTau28_hez_final_12.ptau already exists. Skipping."
-else
-    echo 'Downloading powersOfTau28_hez_final_12.ptau'
-    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_12.ptau
-fi
-
 echo "Compiling Multipler3.circom..."
 
 # compile circuit
@@ -21,25 +14,39 @@ if [[ $* == *--nodejs* ]]
 then
     echo "Generating nodejs version of Multipler3.sol..."
     cd Multipler3/circuit_js
+    node generate_witness.js multipler3.wasm ../input.json witness.wtns
 
-# # Start a new zkey and make a contribution
-node generate_witness.js multiplier2.wasm input.json witness.wtns
+else
+    echo "Using cpp version of Multipler3.sol..."
+    cd Multipler3/circuit_cpp
+    make
 
-snarkjs powersoftau new bn128 12 pot12_0000.ptau -v
-snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v
-snarkjs powersoftau prepare phase2 pot12_0001.ptau pot12_final.ptau -v
-snarkjs groth16 setup multiplier2.r1cs pot12_final.ptau multiplier2_0000.zkey
-snarkjs zkey contribute multiplier2_0000.zkey multiplier2_0001.zkey --name="1st Contributor Name" -v
-snarkjs zkey export verificationkey multiplier2_0001.zkey verification_key.json
-snarkjs groth16 prove multiplier2_0001.zkey witness.wtns proof.json public.json
-snarkjs groth16 verify verification_key.json public.json proof.json
+    Multipler3/circuit ../input.json ../witness.wtns
+fi
 
-
-snarkjs groth16 setup Multipler3/Multipler3.r1cs powersOfTau28_hez_final_10.ptau Multipler3/circuit_0000.zkey
-snarkjs zkey contribute Multipler3/multiplier3_0000.zkey Multipler3/multiplier3_final.zkey --name="1st Contributor Name" -v -e="random text"
-snarkjs zkey export verificationkey Multipler3/multiplier3_final.zkey Multipler3/verification_key.json
-
-# # generate solidity contract
-snarkjs zkey export solidityverifier Multipler3/multiplier3_final.zkey ../Multipler3Verifier.sol
-
+cp ../witness.wtns ../../../witness.wtns
 cd ../..
+
+# create new powersoftau ceremony
+snarkjs powersoftau new bn128 12 pot12_0000.ptau -v
+
+# contribute to the created ceremony
+snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v
+
+#2. Phase 2
+# create new powersoftau ceremony
+snarkjs powersoftau prepare phase2 pot12_0001.ptau pot12_final.ptau -v
+
+# Generate zkey files
+snarkjs groth16 setup Multiplier3/multiplier3.r1cs pot12_final.ptau Multiplier3/multiplier3_0000.zkey
+snarkjs zkey contribute Multiplier3/multiplier3_0000.zkey Multiplier3/multiplier3_0001.zkey --name="1st Contributor Name" -v
+
+# Export verification key to json file
+snarkjs zkey export verificationkey Multiplier3/multiplier3_0001.zkey Multiplier3/verification_key.json
+
+# genarate zero knowledge proof using zkey and witness
+snarkjs groth16 prove Multiplier3/multiplier2_0001.zkey Multiplier3/witness.wtns Multiplier3/proof.json Multiplier3/public.json
+
+# use the verification key proof and public file to verify if proof is valid
+snarkjs groth16 verify Multiplier3/verification_key.jsonMultiplier3/public.json Multiplier3/proof.json
+
