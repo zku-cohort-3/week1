765a33538d647cba5c4cf25d4c1a4189c36c636f
João Henrique Costa <joaoh9@pm.me>
2022-05-08 23:07:10 -0300

    Update compile-Multiplier3-groth16.sh

--- Q2/scripts/compile-Multiplier3-groth16.sh
+++ Q2/scripts/compile-Multiplier3-groth16.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-# [assignment] create your own bash script to compile Multipler3.circom modeling after compile-HelloWorld.sh below
+# [assignment] create your own bash script to compile Multiplier3.circom modeling after compile-HelloWorld.sh below
