299d43b82221a26efe733839b96b1e6a77252f96
Ishan Pandey <191.imp@gmail.com>
2022-05-12 01:13:21 +0530

    Week 1 Assignment

--- Q2/scripts/bump-solidity.js
+++ Q2/scripts/bump-solidity.js
@@ -10,3 +10,20 @@ bumped = bumped.replace(verifierRegex, 'contract HelloWorldVerifier');
 fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
 
 // [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
+
+// Fetching contents of the Multiplier3Verifier.sol file
+let content_groth16 = fs.readFileSync("./contracts/Multiplier3Verifier.sol", { encoding: 'utf-8' });
+
+// Replacing all expressions which matches the solidityRegex regular expression by 'pragma solidity ^0.8.0'
+let bumped_groth16 = content_groth16.replace(solidityRegex, 'pragma solidity ^0.8.0');
+bumped_groth16 = bumped_groth16.replace(verifierRegex, 'contract Verifier');
+
+// Writing back the updated smart contract to the .sol file
+fs.writeFileSync("./contracts/Multiplier3Verifier.sol", bumped_groth16);
+
+// Same steps for _plonk contract
+let content_plonk = fs.readFileSync("./contracts/_plonkMultiplier3Verifier.sol", { encoding: 'utf-8' });
+bumped_plonk = content_plonk.replace(solidityRegex, 'pragma solidity ^0.8.0');
+bumped_plonk = bumped_plonk.replace(verifierRegex, 'contract PlonkVerifier');
+
+fs.writeFileSync("./contracts/_plonkMultiplier3Verifier.sol", bumped_plonk);
--- Q2/scripts/compile-Multiplier3-plonk.sh
+++ Q2/scripts/compile-Multiplier3-plonk.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
 # [assignment] create your own bash script to compile Multipler3.circom using PLONK below
+cd contracts/circuits
+
+mkdir _plonkMultiplier3
+
+if [ -f ./powersOfTau28_hez_final_10.ptau ]; then
+    echo "powersOfTau28_hez_final_10.ptau already exists. Skipping."
+else
+    echo 'Downloading powersOfTau28_hez_final_10.ptau'
+    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_10.ptau
+fi
+
+echo "Compiling Multiplier3.circom..."
+
+# compile circuit
+
+circom Multiplier3.circom --r1cs --wasm --sym -o _plonkMultiplier3
+snarkjs r1cs info _plonkMultiplier3/Multiplier3.r1cs
+
+# Start a new zkey and make a contribution
+
+snarkjs plonk setup _plonkMultiplier3/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau _plonkMultiplier3/circuit.zkey
+snarkjs zkey export verificationkey _plonkMultiplier3/circuit.zkey _plonkMultiplier3/verification_key.json
+
+# generate solidity contract
+snarkjs zkey export solidityverifier _plonkMultiplier3/circuit.zkey ../_plonkMultiplier3Verifier.sol
+
+cd ../..
