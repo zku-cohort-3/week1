32d24a8d46ff244f4280edc75917024881f11624
Tomosuke0930 <tomotomo09303396@gmail.com>
2022-06-25 10:08:17 +0900

    Error: zkey file is not groth16

--- Q2/scripts/compile-Multiplier3-plonk.sh
+++ Q2/scripts/compile-Multiplier3-plonk.sh
@@ -4,6 +4,7 @@ cd contracts/circuits
 
 mkdir Multiplier3_plonk
 
+# 怪しいところ１
 if [ -f ./powersOfTau28_hez_final_10.ptau ]; then
     echo "powersOfTau28_hez_final_10.ptau already exists. Skipping."
 else
@@ -15,17 +16,25 @@ echo "Compiling Multiplier3_plonk.circom..."
 echo "-------------------------------"
 
 # compile circuit
-
 circom Multiplier3.circom --r1cs --wasm --sym -o Multiplier3
 snarkjs r1cs info Multiplier3_plonk/Multiplier3.r1cs
 
 
 # Start a new zkey and make a contribution
 
+# setupに関わる何かがおかしい
+# snarkjs plonk setup circuit.r1cs pot12_final.ptau circuit_final.zkey
+# final.zkeyのst湯特方法,
+
+###違い plonk key！
 snarkjs plonk setup Multiplier3_plonk/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau Multiplier3_plonk/circuit_final.zkey
-###---  Error -------
-snarkjs zkey verify Multiplier3_plonk/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau Multiplier3_plonk/circuit_final.zkey
-###-------------------
+# snarkjs plonk setup Multiplier3_plonk/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau Multiplier3_plonk/circuit_plonk.zkey
+
+
+
+###---  Error -------ここが不要？
+# ここは18だった
+# snarkjs zkey verify Multiplier3_plonk/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau Multiplier3_plonk/circuit_final.zkey
 snarkjs zkey export verificationkey Multiplier3/circuit_final.zkey Multiplier3/verification_key.json
 
 
